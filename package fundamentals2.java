d7f2e38e4e390622838d75d56328003aef2a32fe
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-05-07 19:28:23 +0530

    Create package fundamentals2.java

---
+++ package fundamentals2.java
@@ -0,0 +1,22 @@
+package fundamentals;
+
+public class helloworld {
+	
+	
+		public static void main(String args[]) {
+			
+			
+				//println print new line at the end
+			
+				System.out.println("Hello world");
+				
+				System.out.println("Hello world");
+				
+				System.out.println("Hello world");
+				
+				//System.out.println("Hello world");
+				//syso (use control + space )
+		}
+	
+
+}
