a822c4a716c56a80c7505e872138fa1b5d7cf4a7
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-05-18 04:34:56 +0530

    Create scanner.java

---
+++ scanner.java
@@ -0,0 +1,21 @@
+package fundamentals;
+
+import java.util.Scanner;
+
+public class Arithemetic_Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b;
+		
+		Scanner s = new Scanner(System.in);
+		
+		a=s.nextInt();
+		b=s.nextInt();
+		
+		int c= a + b;
+		System.out.println(c);
+	}
+
+}
