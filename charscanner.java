045cb2223aedd0f5ce2ec9769639be43525325b1
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-05-18 05:02:02 +0530

    Create charscanner.java

---
+++ charscanner.java
@@ -0,0 +1,30 @@
+package fundamentals;
+
+import java.util.Scanner;
+
+public class Arithemetic_Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		String str= s.next();
+		char ch=str.charAt(0);
+		System.out.println(ch);
+		
+		
+		
+		
+		//		int a,b;
+//		
+//		
+//		
+//		a=s.nextInt();
+//		b=s.nextInt();
+//		
+//		int c= a + b;
+//		System.out.println(c);
+//		
+	}
+
+}
