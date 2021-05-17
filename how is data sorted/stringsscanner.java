1b71c115f5f11a9c3a8e18950eb9c5477f492e87
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-05-18 05:13:21 +0530

    Add files via upload

---
+++ how is data sorted/stringsscanner.java
@@ -0,0 +1,29 @@
+package fundamentals;
+
+import java.util.Scanner;
+
+public class Arithemetic_Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		String str= s.next();
+		System.out.println(str);
+		
+		
+		
+		
+		//		int a,b;
+//		
+//		
+//		
+//		a=s.nextInt();
+//		b=s.nextInt();
+//		
+//		int c= a + b;
+//		System.out.println(c);
+//		
+	}
+
+}
